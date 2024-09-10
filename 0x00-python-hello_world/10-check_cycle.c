4108ef7819a1ed7cd8c52931f4571dc5b670ea0f
Vasitha Yong <sulemfelsi@gmail.com>
2024-09-09 18:10:29 -0700

    Write a Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "list.h"
+
+/**
+ * check_cycle - Function that checks alinked-list for a loop
+ * @list: the list of items to be checked
+ *
+ * Return: 0 if there is no cycle and 1 otherwise
+ */
+int check_cycle(listint_t *list)
+
