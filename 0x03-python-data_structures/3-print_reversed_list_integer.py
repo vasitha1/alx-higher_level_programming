e3783c353a60e80ff950686d567b256de79508d6
Vasitha Yong <sulemfelsi@gmail.com>
2024-09-16 15:46:41 -0700

    Fixing type specifier for task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
     reversed_my_list = reversed(my_list)
 
     for item in reversed_my_list:
-        print("{}".format(item))
+        print("{:d}".format(item))
