138a463c2a9007cd7da61e2b988ab595e3f4037b
Vasitha Yong <sulemfelsi@gmail.com>
2024-09-16 16:01:10 -0700

    Using .copy method instead of a loop

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,11 +6,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list_copy)
 
-    for item in range(len(my_list) - 1):
-
-        if item == idx:
-            my_list_copy.append(element)
-        else:
-            my_list_copy.append(my_list[item])
+    my_list_copy = my_list.copy()
+    my_list_copy[idx] = element
 
     return (my_list_copy)
