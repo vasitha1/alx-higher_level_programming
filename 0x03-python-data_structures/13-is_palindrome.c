f0f2d6e7aa57bd0f8b21c2f9139ab43e37e4d70d
Vasitha Yong <sulemfelsi@gmail.com>
2024-09-16 19:13:55 -0700

    Task 13: Complete the source code in order to switch value of a and b

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stddef.h>
 
 /**
  * is_palindrome - Function that chech if a linked list is a palindrome
@@ -8,14 +10,31 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *temp;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+
 	if (*head == NULL || head == NULL)
 		return (1);
 
-	while (*head->next->next != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
-		if (*head->next->n + *head->n != *head->next->next->n)
-			return (0);
-		*head->next->next = head->next->next->next;
+		fast = fast->next->next;
+		temp = slow;
+		slow = slow->next;
+		temp->next = prev;
+		prev = temp;
 	}
-	return (1)
+	if (fast != NULL)
+		slow = slow->next;
+
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+		prev = prev->next;
+		slow = slow->next;
+	}
+	return (1);
 }
