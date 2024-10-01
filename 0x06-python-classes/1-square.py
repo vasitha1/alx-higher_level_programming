78d865eeedfa24088d53ae85df7bea4835082ceb
Vasitha Yong <sulemfelsi@gmail.com>
2024-10-01 15:05:24 -0700

    Class Square that defines a square by private instance attribute: size with no type verification

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square by private instance attribute: size"""
+
+
+class Square:
+
+    """Innit innitialises the class"""
+
+    def __init__(self, size=0):
+        self.__size = size
