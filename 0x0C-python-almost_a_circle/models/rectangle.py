78665243d4162cb44c689382e070d0d238711e4e
vasitha1 <sulemfelsi@gmail.com>
2024-10-21 15:09:39 -0700

    Fixing the value checked in setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,7 +40,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
-        if width <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
 
         self.__width = value
@@ -56,7 +56,7 @@ class Rectangle(Base):
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
 
-        if Height <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -71,7 +71,7 @@ class Rectangle(Base):
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
 
-        if x < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -86,7 +86,7 @@ class Rectangle(Base):
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
 
-        if y < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
