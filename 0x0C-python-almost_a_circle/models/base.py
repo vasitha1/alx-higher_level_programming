26c430b50f50deff0956f14d0ccc5e12e2f03d3b
vasitha1 <sulemfelsi@gmail.com>
2024-10-21 18:03:19 -0700

    Adding static method decorators on file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Displays the JSON representation of a list"""
         if list_dictionaries is None or list_dictionaries == []:
@@ -34,6 +35,7 @@ class Base:
         with open(filename, "w") as f:
             f.write(json_string)
 
+    @staticmethod
     def from_json_string(json_string):
         """Method that returns the list representation of a json list"""
         if json_string is None or json_string == []:
