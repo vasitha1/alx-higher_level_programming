a7807bb5bedab60546dd7192fca1c845e885fa2e
vasitha1 <sulemfelsi@gmail.com>
2024-11-21 23:27:29 -0800

    Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
    Each record should display: <TV Show genre> - <Number of shows linked to this genre>
    First column must be called genre
    Second column must be called number_of_shows
    Don’t display a genre that doesn’t have any shows linked
    Results must be sorted in descending order by the number of shows linked
    You can use only one SELECT statement
    The database name will be passed as an argument of the mysql command

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+--  all genres from hbtn_0d_tvshows and displays number of shows linked to each
+
+SELECT tv_genres.name AS genre,
+	COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
