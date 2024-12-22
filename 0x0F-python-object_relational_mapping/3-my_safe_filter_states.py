76b440c5d8867fc112ae116b80990d8ef4334870
vasitha1 <sulemfelsi@gmail.com>
2024-12-22 10:48:41 -0800

    Protecting database from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+def main():
+    searched_name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    query = "SELECT id, name FROM states WHERE name LIKE BINARY %s"
+    cursor.execute(query, (searched_name,))
+
+    results = cursor.fetchall()
+
+    if results:
+        for row in results:
+            print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
