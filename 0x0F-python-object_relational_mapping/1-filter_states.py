10cdf8ad9eb469e0e4f8e8761c0d4bc39b1ac231
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 02:15:32 -0800

    fixing 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,19 +8,21 @@ import sys
 
 def main():
     """This is the main function"""
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db.cursor()
-    cursor.execute("""SELECT id, name FROM states WHERE name
-                   LIKE 'N%' ORDER BY id ASC""")
-    results = cursor.fetchall()
 
-    if results:
-        for row in results:
-            print(row)
+    if len(sys.argv) == 4:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cursor = db.cursor()
+        cursor.execute("""SELECT id, name FROM states WHERE name
+                       LIKE 'N%' ORDER BY id ASC""")
+        results = cursor.fetchall()
 
-    cursor.close()
-    db.close()
+        if results:
+            for row in results:
+                print(row)
+
+        cursor.close()
+        db.close()
 
 
 if __name__ == "__main__":
