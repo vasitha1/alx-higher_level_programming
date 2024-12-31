cd68ce940bc84e884bc4eee3f94ffb407dbc0c4e
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 04:00:46 -0800

    Doccumenting 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""
+This model prints a model which is safe from sql injections
+"""
 import MySQLdb
 import sys
 
 
 def main():
+    """The main function"""
     searched_name = sys.argv[4]
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
