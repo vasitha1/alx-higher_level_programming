5e908c4a26d905024176c620173a1bfc776c8b63
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 06:16:18 -0800

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,13 +5,16 @@ This model creates a class base and linkes it to the database
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+
 Base = declarative_base()
 
+
 class State(Base):
-   """
-   Defines the state class to represent the base table
-   """
+    """
+    Defines the state class to represent the base table
+    """
     __tablename__ = 'states'
 
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
