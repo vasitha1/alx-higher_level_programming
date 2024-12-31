22eee2b333a329d93c44fca0834ddda5fc4c2f29
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 01:54:58 -0800

    Doccumenting 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""
+This model is meant to print states and their corresponding ids
+"""
 import MySQLdb
 import sys
 
 
 def main():
+    """This is the main function"""
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
