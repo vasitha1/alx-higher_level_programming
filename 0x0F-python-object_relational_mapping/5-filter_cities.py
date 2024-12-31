45f53f78cda2e5c30e5d4152b072e7faf62f8da5
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 04:14:58 -0800

    Doccumenting 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
+"""
+This module takes in the name of a state as an argument and lists all cities
+of that state, using the database hbtn_0e_4_usa
+"""
 import MySQLdb
 import sys
 
 
 def main():
+    """The main function"""
     searched_name = sys.argv[4]
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
