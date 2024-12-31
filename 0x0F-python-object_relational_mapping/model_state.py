75d1c4ef54630a3c2dc806e8072d64580fa07371
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 06:01:53 -0800

    Fixing linking in model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,15 +3,15 @@
 This model creates a class base and linkes it to the database
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 class State(Base):
    """
-   Defines the base class
+   Defines the state class to represent the base table
    """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
+
+    id = Column(Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
