4d2b72728adda97cad5f4b25d2badd15752b1134
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 05:55:13 -0800

    Fixing linking in model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, MetaData
 
 mymetadata = MetaData()
-Base = declarative_base(mymetadata)
+Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
    """
