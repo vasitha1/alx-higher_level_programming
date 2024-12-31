39bb4a8e3b36b97fcbcd904917f5d325c499b040
vasitha1 <sulemfelsi@gmail.com>
2024-12-31 01:30:19 -0800

    Doccumenting 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""
+This module selects id and name from the state database
+"""
 import MySQLdb
 import sys
 
 
 def main():
+    """ Main function that runs when the file is run"""
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
