d1883bafda2ca70d6046f8438a43dcef6937d72c
vasitha1 <sulemfelsi@gmail.com>
2025-01-17 21:43:25 +0000

    fixing pycodestyle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id
 variable found in the header of the response.
 """
 
@@ -20,6 +21,7 @@ def main():
         request_id = response.getheader('X-Request-Id')
 
         print('{}'.format(request_id))
+
 
 if __name__ == "__main__":
     main()
