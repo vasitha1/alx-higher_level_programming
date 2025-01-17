f433bcc382c8d31273d04b88d8e7acefe571d234
vasitha1 <sulemfelsi@gmail.com>
2025-01-17 19:07:59 +0000

    Making sure that script does not run if it is imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,17 @@ This scrip fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
+def main():
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status'
+    ) as response:
+        html = response.read()
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+        print("Body response:")
+        print("\t-type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
-    print("Body response:")
-    print("\t-type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+if __name__ == "__main__":
+    main()
